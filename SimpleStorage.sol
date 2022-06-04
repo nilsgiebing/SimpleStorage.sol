3bb8e9cfd3a3471a8079988812eb98e673471412
Nils Giebing <11206675+nilsgiebing@users.noreply.github.com>
2022-06-04 22:59:35 +0200

    Add files via upload
    Added a new struct and array.

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,19 +1,34 @@
-// add a license to the smart contract
+// adding a license to the smart contract
 // SPDX-License-Identifier: GPL-3.0
 
-// add the solidity version
+// adding the solidity version
 pragma solidity ^0.8.0;
 
 //first smart contract "SimpleContract"
 contract SimpleContract {
-    //sets "favoriteNumber" to 5
+    //setting "favoriteNumber" to 5
     uint256 favoriteNumber = 5;
-    // create a function which sets the variable "favoriteNumber" to a number the user enters
+
+    //creating a struct of people
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // creating a public People array
+    People[] public people;
+
+    // creating a function which sets the variable "favoriteNumber" to a number the user enters
     function store(uint256 _favoritenumber) public {
         favoriteNumber = _favoritenumber;
     }
-    //create a function to retrieve/get the updated "favoriteNumber" variable
+    //creating a function to retrieve/get the updated "favoriteNumber" variable
     function retrieve() public view returns(uint256) {
         return favoriteNumber;
+    }
+    //creating a function to add a new person to the people array
+    function addPeople(string memory _name, uint256 _favoritenumber) public {
+        People memory newPerson = People({favoriteNumber: _favoritenumber, name: _name});
+        people.push(newPerson);
     }
 }
